b8fa96a7ac7f843dd6addf4f65e480aa680ffb7c
Zenovs <34447646+Zenovs@users.noreply.github.com>
2026-01-03 17:37:23 +0100

    next datei angepasst

--- nextjs_space/types/next-auth.d.ts
+++ nextjs_space/types/next-auth.d.ts
@@ -6,6 +6,7 @@ declare module 'next-auth' {
     email: string;
     name?: string | null;
     role?: string;
+    image?: string | null;
   }
 
   interface Session {
@@ -14,6 +15,7 @@ declare module 'next-auth' {
       email: string;
       name?: string | null;
       role?: string;
+      image?: string | null;
     };
   }
 }
@@ -22,5 +24,6 @@ declare module 'next-auth/jwt' {
   interface JWT {
     id: string;
     role?: string;
+    image?: string | null;
   }
 }
